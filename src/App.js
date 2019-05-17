c17168189302e7a18d3eb9db2d76f7195078c3f8
anwar magara <sadatt254@gmail.com>
2019-05-17 11:41:25 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IonContent, IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
+import '@ionic/core/css/ionic.bundle.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+          <IonFab vertical="bottom" horizontal="end" slot="fixed">
+            <IonFabButton>
+              <IonIcon name="arrow-dropleft" />
+            </IonFabButton>
+          </IonFab>
     </div>
   );
 }
